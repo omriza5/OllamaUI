d677cec6825330d2fdf6e73fc3f8bcc8b392e82c
omri zaher <omriza5@gmail.com>
2025-09-17 08:57:06 +0300

    fix: refactor requestPrediction to use publishToExchange for message publishing

--- src/utils/producer.js
+++ src/utils/producer.js
@@ -11,6 +11,22 @@ export async function publishToQueue(queue, message) {
   await connection.close();
 }
 
+export async function publishToExchange(exchangeName, routingKey, message) {
+  const connection = await amqp.connect(
+    process.env.RABBITMQ_URL || "amqp://localhost"
+  );
+  const channel = await connection.createChannel();
+
+  // Declare the exchange
+  await channel.assertExchange(exchangeName, "direct", { durable: true });
+
+  // Publish to exchange with routing key
+  channel.publish(exchangeName, routingKey, Buffer.from(message));
+
+  await channel.close();
+  await connection.close();
+}
+
 export async function requestPrediction(
   chatId,
   imageName,
@@ -24,12 +40,46 @@ export async function requestPrediction(
     predictionUid, // Include UID in message
   };
 
-  // Send to queue
-  await publishToQueue("predictions", JSON.stringify(message));
+  try {
+    // Send to queue
+    await publishToExchange(
+      "YoloAppUserEvents",
+      "predictions",
+      JSON.stringify(message)
+    );
 
-  // Return UID immediately for polling
-  return {
-    status: "queued",
-    predictionUid: predictionUid, // Frontend can start polling this UID
-  };
+    await publishToExchange(
+      "YoloAppUserEvents",
+      "analytics",
+      JSON.stringify({
+        publish_event: "analytics_event",
+        event: "new_prediction_ANALYTICS",
+        chatId: chatId,
+        imageName: imageName,
+        user_id: userId,
+        predictionUid: predictionUid,
+      })
+    );
+
+    await publishToExchange(
+      "YoloBroadcast",
+      "billing",
+      JSON.stringify({
+        publish_event: "billing_event",
+        event: "new_prediction_BILLING",
+        chatId: chatId,
+        imageName: imageName,
+        user_id: userId,
+        predictionUid: predictionUid,
+      })
+    );
+
+    // Return UID immediately for polling
+    return {
+      status: "queued",
+      predictionUid: predictionUid, // Frontend can start polling this UID
+    };
+  } catch (error) {
+    console.error("Error publishing to exchange:", error);
+  }
 }
